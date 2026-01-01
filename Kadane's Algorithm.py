8baf1247e0637aa542d4e41e75c1d524a9f64e5e
Surya Sharma <137315744+surya775577@users.noreply.github.com>
2026-01-01 12:47:02 +0530

    Create Kadane's Algorithm.py

---
+++ Kadane's Algorithm.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubarraySum(self, arr):
+        curr_sum=arr[0]
+        max_sum=arr[0]
+        
+        for i in range(1,len(arr)):
+            curr_sum=max(arr[i],curr_sum+arr[i])
+            max_sum=max(curr_sum,max_sum)
+        return max_sum
