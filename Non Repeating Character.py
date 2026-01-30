61f9114909e8b98616b7148a647c03a60f4c4e75
Surya Sharma <137315744+surya775577@users.noreply.github.com>
2026-01-30 20:51:03 +0530

    Create Non Repeating Character.py

---
+++ Non Repeating Character.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nonRepeatingChar(self,s):
+        freq={}
+        
+        for ch in s:
+            freq[ch]=freq.get(ch,0)+1
+            
+        for ch in s:
+            if freq[ch]==1:
+                return ch
+                
+        return'$'
